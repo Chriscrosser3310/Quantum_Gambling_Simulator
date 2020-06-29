4785fdc0fd1ff4cb53f20f9c5d660d2fe8de7fa5
ChrisW918 <67126937+ChrisW918@users.noreply.github.com>
2020-06-29 02:29:19 -0700

    move Class Knob from StartStage to SharedClasses

--- game_lib/SharedClasses.py
+++ game_lib/SharedClasses.py
@@ -172,3 +172,73 @@ class TutorialBlock():
     
     def __init__(self, pos, message):
         pass
+
+class Knob():
+    width = 50
+    height = 50
+
+    def __init__(self, pos, angle):
+
+        self.original_image = pygame.transform.scale(pygame.image.load(f'{IMAGE_PATH}/knob.jpg'), 
+                                            (self.width, self.height))     
+        self.image = self.original_image
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+        self.angle = angle
+        
+        self.update_text("{:03.1f}π".format(1 * self.angle))  # self.text
+        self.text_rect = self.text.get_rect()
+        self.text_rect.center = (self.rect.centerx, self.rect.bottom - self.height - 20)
+
+        self.clickable = True
+        self.click = False
+
+    def check_click(self, pos):
+        if self.rect.left <= pos[0] and self.rect.right >= pos[0]:
+            if self.rect.bottom - self.height <= pos[1] and self.rect.bottom >= pos[1]:
+                self.click = True
+                pygame.mouse.get_rel()
+
+    def update_rotation(self):
+        self.image = pygame.transform.rotate(self.original_image, self.angle*180/pi)
+        x, y = self.rect.center  # Save its current center.
+        self.rect = self.image.get_rect()  # Replace old rect with new rect.
+        self.rect.center = (x, y)  # Put the new rect's center at old center.
+    
+    def update_text(self, message):
+        font = pygame.font.SysFont('timesnewroman', 30)
+        self.text = font.render(message, True, pygame.Color("black"))
+    
+    def update_drag(self):
+        if self.click:
+            #Dx = abs(pygame.mouse.get_pos()[0] - self.original_image.get_rect().center[0])
+            #Dy = abs(pygame.mouse.get_pos()[1] - self.original_image.get_rect().center[1])
+            dy = pygame.mouse.get_rel()[1]
+            # both dy, dx are negative, so -*- = +
+            angle = -dy/30#/Dy if Dy != 0 else 0
+    
+            self.angle += angle
+            
+            if self.angle < 0:
+                self.angle = 0
+            if self.angle > pi:
+                self.angle = pi
+                
+            self.update_rotation()
+            self.update_text("{:03.2f}π".format(self.angle/pi))
+
+    def update_with_angle(self, angle):
+        
+        self.angle = angle
+        if self.angle < 0:
+            self.angle = 0
+        if self.angle > pi:
+            self.angle = pi
+            
+        self.update_rotation()
+        self.update_text("{:03.2f}π".format(self.angle/pi))
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        surface.blit(self.text, self.text_rect)
